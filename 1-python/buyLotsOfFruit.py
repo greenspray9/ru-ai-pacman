edb32b96300f47e460ee6711e04b2122fc09c6de
nickoliver <nick.stracke@web.de>
2017-04-10 12:29:21 +0200

    made robust

--- 1-python/buyLotsOfFruit.py
+++ 1-python/buyLotsOfFruit.py
@@ -3,20 +3,22 @@ fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
 
 
 def buyLotsOfFruit(orderList):
-    """
-        orderList: List of (fruit, numPounds) tuples
 
-    Returns cost of order
-    """
     totalCost = 0;
     for tuple in orderList:
-    	quantity = tuple[1]
-    	name 	 = tuple[0]
-    	perFruitPrice = fruitPrices[name]
-    	totalCost = totalCost + quantity * perFruitPrice
+        quantity = tuple[1]
+        name 	 = tuple[0]
+
+        if quantity < 0:
+            print('wrong quantity of', quantity)
+        elif not(name in fruitPrices):
+            print(name ,'not in list')
+        else:
+            perFruitPrice = fruitPrices[name]
+            totalCost = totalCost + quantity * perFruitPrice
 
     return totalCost
 
 
-orderList = [('apples', 2), ('pears', 3), ('limes', 4)]
+orderList = [('apples', 2), ('pears', 3), ('limes', 4), ('apples', -10), ('pineapple', 10)]
 print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
